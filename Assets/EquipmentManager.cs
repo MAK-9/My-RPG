fa812e0843ca0b08814b340749a90b9a7228883c
MAK-9 <70964650+MAK-9@users.noreply.github.com>
2021-06-29 14:22:09 +0200

    add unequip button

---
+++ Assets/EquipmentManager.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour
+{
+    #region Singleton
+    public static EquipmentManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    private Equipment[] currentEquipment;
+
+    private Inventory inventory;
+
+    private void Start()
+    {
+        inventory = Inventory.instance;
+        int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
+        currentEquipment = new Equipment[numSlots];
+    }
+
+    public void Equip(Equipment newItem)
+    {
+        int slotIndex = (int) newItem.equipSlot;
+
+        Equipment oldItem = null;
+        if (currentEquipment[slotIndex] != null)
+        {
+            oldItem = currentEquipment[slotIndex];
+            inventory.Add(oldItem);
+        }
+        currentEquipment[slotIndex] = newItem;
+    }
+
+    public void Unequip(int slotIndex)
+    {
+        if (currentEquipment[slotIndex] != null)
+        {
+            Equipment oldItem = currentEquipment[slotIndex];
+            inventory.Add(oldItem);
+
+            currentEquipment[slotIndex] = null;
+        }
+    }
+
+    public void UnequipAll()
+    {
+        Debug.Log("Unequiping all");
+        for (int i = 0; i < currentEquipment.Length; i++)
+        {
+            Unequip(i);
+        }
+    }
+
+    public void OnUnequipAll()
+    {
+        UnequipAll();
+    }
+}

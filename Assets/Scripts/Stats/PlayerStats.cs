16d851de775f27736c09bdeb2d3da87459ee726c
MAK-9 <70964650+MAK-9@users.noreply.github.com>
2021-06-30 12:22:51 +0200

    add player/enemy attack

--- Assets/Scripts/Stats/PlayerStats.cs
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -23,4 +23,11 @@ public class PlayerStats : CharacterStats
             damage.RemoveModifier(oldItem.damageModifier);
         }
     }
+
+    public override void Die()
+    {
+        base.Die();
+        PlayerManager.instance.KillPlayer();
+
+    }
 }
